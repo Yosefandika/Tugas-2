480b0df84db05f33763d2960d7b0324feee89331
Yosefandika <151033131+Yosefandika@users.noreply.github.com>
2023-11-18 11:35:48 +0700

    Add files via upload

---
+++ ape.php
@@ -0,0 +1,11 @@
+// ape.php
+
+<?php
+require_once 'animal.php';
+
+class Ape extends Animal {
+    public function yell() {
+        echo "Auooo";
+    }
+}
+?>
---
+++ frog.php
@@ -0,0 +1,11 @@
+// frog.php
+
+<?php
+require_once 'animal.php';
+
+class Frog extends Animal {
+    public function jump() {
+        echo "hop hop";
+    }
+}
+?>
